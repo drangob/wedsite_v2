7fef058f41779091c4512c9c47dd9edf7337e5ca
Daniel Horbury <daniel.horbury@multiverse.io>
2024-08-05 09:07:59 +0100

    fix user role migration & schema

--- prisma/migrations/20240803125804_user_roles/migration.sql
+++ prisma/migrations/20240803125804_user_roles/migration.sql
@@ -2,4 +2,4 @@
 CREATE TYPE "Role" AS ENUM ('GUEST', 'ADMIN');
 
 -- AlterTable
-ALTER TABLE "Session" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'GUEST';
+ALTER TABLE "User" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'GUEST';
