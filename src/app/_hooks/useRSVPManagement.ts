ae6615af436e9abb06532c1e732ed4f3e20f3ee0
Daniel Horbury <daniel.horbury@multiverse.io>
2024-09-05 17:38:21 +0100

    add functioning RSVP management admin page - sans sorting

--- src/app/_hooks/useRSVPManagement.ts
+++ src/app/_hooks/useRSVPManagement.ts
@@ -1,13 +1,24 @@
 import { api } from "@/trpc/react";
 import { useDebounce } from "use-debounce";
 import { useState } from "react";
+import { type z } from "zod";
+import {
+  type rsvpRouter,
+  type GuestRSVPSchema,
+  type RSVPSchema,
+} from "@/server/api/routers/rsvp";
+import toast from "react-hot-toast";
+import { type TRPCClientErrorLike } from "@trpc/client";
+
+export type RSVP = z.infer<typeof RSVPSchema>;
+export type GuestRSVP = z.infer<typeof GuestRSVPSchema>;
 
 export const useRSVPManagement = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [debouncedSearchTerm] = useDebounce(searchTerm, 300);
 
-  const { data, isLoading, error, fetchNextPage, hasNextPage } =
-    api.rsvp.getRSVPs.useInfiniteQuery(
+  const { data, isLoading, error, fetchNextPage, hasNextPage, refetch } =
+    api.rsvp.getUserRSVPs.useInfiniteQuery(
       {
         limit: 30,
         search: debouncedSearchTerm,
@@ -15,15 +26,37 @@ export const useRSVPManagement = () => {
       { getNextPageParam: (lastPage) => lastPage.nextCursor },
     );
 
-  const rsvps = data?.pages.flatMap((page) => page.items) ?? [];
+  const commonMutationConfig = {
+    onSuccess: async () => {
+      await refetch();
+      toast.success("RSVP updated successfully");
+    },
+    onError: (error: TRPCClientErrorLike<typeof rsvpRouter>) =>
+      toast.error(error.message),
+  };
+
+  const upsertGuestRSVPMutation =
+    api.rsvp.upsertGuestRSVP.useMutation(commonMutationConfig);
+
+  const guestRSVPs = data?.pages.flatMap((page) => page.items) ?? [];
+
+  const upsertGuestRSVP = (guestRSVP: GuestRSVP) => {
+    upsertGuestRSVPMutation.mutate({
+      userId: guestRSVP.guest.id,
+      isAttending: guestRSVP.rsvp?.isAttending ?? false,
+      dietaryRequirements: guestRSVP.rsvp?.dietaryRequirements ?? "",
+      extraInfo: guestRSVP.rsvp?.extraInfo ?? "",
+    });
+  };
 
   return {
-    rsvps,
+    guestRSVPs,
     isLoading,
     error,
     fetchNextPage,
     hasNextPage,
     searchTerm,
     setSearchTerm,
+    upsertGuestRSVP,
   };
 };
