9ef5d33a20d790a3fbd27b59cd8475a6533212d1
Daniel Horbury <daniel.horbury@multiverse.io>
2024-09-28 14:37:45 +0100

    Make email list clickable

--- src/app/admin/(admin-pages)/email/EmailList.tsx
+++ src/app/admin/(admin-pages)/email/EmailList.tsx
@@ -2,26 +2,33 @@
 
 import moment from "moment";
 import React from "react";
-import { Card, CardBody, CardFooter, Chip } from "@nextui-org/react";
+import { Button, Card, CardBody, CardFooter, Chip } from "@nextui-org/react";
+import { CircleStopIcon, PlusCircleIcon } from "lucide-react";
+import { type Email } from "./page";
 
-interface Email {
-  subject: string;
-  updatedAt: Date;
-  sent: boolean;
+interface EmailCardProps {
+  email: Email;
+  isSelected: boolean;
+  onClick: () => void;
 }
 
-const EmailCard: React.FC<Email> = ({ subject, updatedAt, sent }) => {
+const EmailCard: React.FC<EmailCardProps> = ({
+  email: { subject, updatedAt, sent },
+  isSelected,
+  onClick,
+}) => {
   return (
     <Card
       isPressable
       isHoverable
       shadow="none"
-      className="flex-shrink-0 border-2 border-solid border-gray-500"
+      className={`flex-shrink-0 border-2 border-solid border-gray-500 ${isSelected ? "bg-gray-200" : ""}`}
+      onClick={onClick}
     >
-      <CardBody className="h-16 w-52 flex-shrink-0">
-        <div className="flex flex-shrink-0 justify-between">
-          <h3 className="text-xs font-semibold">
-            {subject.length > 55 ? subject.slice(0, 52) + "..." : subject}
+      <CardBody className="h-16 w-52 flex-shrink-0 overflow-hidden">
+        <div className="flex flex-shrink-0 justify-between ">
+          <h3 className={`text-xs ${isSelected ? "font-semibold" : ""}`}>
+            {subject.length > 45 ? subject.slice(0, 42) + "..." : subject}
           </h3>
           <Chip color={sent ? "success" : "warning"} className="ml-2">
             {sent ? "Sent" : "Draft"}
@@ -39,16 +46,55 @@ const EmailCard: React.FC<Email> = ({ subject, updatedAt, sent }) => {
 
 interface EmailListProps {
   emails: Email[];
+  selectedEmail?: Email;
+  setSelectedEmail: (email: Email) => void;
 }
 
-const EmailList: React.FC<EmailListProps> = ({ emails }) => {
+const EmailList: React.FC<EmailListProps> = ({
+  emails,
+  selectedEmail,
+  setSelectedEmail,
+}) => {
+  const addEmail = () => {
+    console.log("Add email");
+  };
+
+  const deleteEmail = () => {
+    console.log("Delete email");
+  };
+
   return (
     <Card className="lg:h-full lg:w-1/4">
       <CardBody className="flex flex-row gap-1 overflow-scroll lg:flex-col">
         {emails.map((email) => (
-          <EmailCard key={email.subject} {...email} />
+          <EmailCard
+            key={email.subject}
+            email={email}
+            isSelected={selectedEmail?.id === email.id}
+            onClick={() => setSelectedEmail(email)}
+          />
         ))}
       </CardBody>
+      <CardFooter className="flex justify-center gap-2">
+        <Button
+          variant="solid"
+          color="primary"
+          isIconOnly
+          onClick={addEmail}
+          aria-label="Add email"
+        >
+          <PlusCircleIcon />
+        </Button>
+        <Button
+          variant="solid"
+          color="danger"
+          isIconOnly
+          onClick={deleteEmail}
+          aria-label="Delete email"
+        >
+          <CircleStopIcon />
+        </Button>
+      </CardFooter>
     </Card>
   );
 };
