0d88a13c51f35cd4d6343ba27efaa2cb5d99622a
Daniel Horbury <daniel.horbury@multiverse.io>
2024-09-29 15:04:15 +0100

    Hook EmailClient and EmailList up to the DB

---
+++ src/app/admin/(admin-pages)/email/EmailClient.test.tsx
@@ -0,0 +1,82 @@
+import { describe, it, expect, vi, type Mock, beforeEach } from "vitest";
+import { render, fireEvent, screen } from "@testing-library/react";
+import EmailClient from "./EmailClient";
+
+// mock debounce so we don't have to wait for the debounce time
+vi.mock("use-debounce", () => ({
+  useDebounce: (email: string, debounceTime: number) => [email, debounceTime],
+}));
+
+describe("EmailClient", () => {
+  const mockEmail = {
+    id: "2",
+    subject: "Test Email 2",
+    body: "Some body text",
+    updatedAt: new Date(),
+    sent: false,
+  };
+  let updateEmail: Mock;
+  let sendEmail: Mock;
+
+  beforeEach(() => {
+    updateEmail = vi.fn();
+    sendEmail = vi.fn();
+  });
+
+  it("renders email client correctly", () => {
+    render(
+      <EmailClient
+        email={mockEmail}
+        updateEmail={updateEmail}
+        sendEmail={sendEmail}
+      />,
+    );
+    expect(screen.getByLabelText("Subject:")).toHaveValue(mockEmail.subject);
+    expect(screen.getByLabelText("Body:")).toHaveValue(mockEmail.body);
+  });
+
+  it("calls updateEmail when the subject is changed", () => {
+    render(
+      <EmailClient
+        email={mockEmail}
+        updateEmail={updateEmail}
+        sendEmail={sendEmail}
+      />,
+    );
+    const newSubject = "New Subject";
+    fireEvent.change(screen.getByLabelText("Subject:"), {
+      target: { value: newSubject },
+    });
+
+    expect(updateEmail).toHaveBeenCalledWith(newSubject, mockEmail.body);
+  });
+
+  it("calls updateEmail when the body is changed", () => {
+    render(
+      <EmailClient
+        email={mockEmail}
+        updateEmail={updateEmail}
+        sendEmail={sendEmail}
+      />,
+    );
+    const newBody = "New Body";
+    fireEvent.change(screen.getByLabelText("Body:"), {
+      target: { value: newBody },
+    });
+
+    expect(updateEmail).toHaveBeenCalledWith(mockEmail.subject, newBody);
+  });
+
+  it("calls sendEmail when the send button is clicked", () => {
+    render(
+      <EmailClient
+        email={mockEmail}
+        updateEmail={updateEmail}
+        sendEmail={sendEmail}
+      />,
+    );
+    fireEvent.click(screen.getByText("Send"));
+
+    expect(sendEmail).toHaveBeenCalled();
+  });
+});
--- src/app/admin/(admin-pages)/email/page.tsx
+++ src/app/admin/(admin-pages)/email/page.tsx
@@ -3,24 +3,70 @@
 import { api } from "@/trpc/react";
 import EmailClient from "./EmailClient";
 import EmailList from "./EmailList";
-import { use, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useRef } from "react";
+import toast from "react-hot-toast";
+import { type TRPCClientErrorLike } from "@trpc/client";
+import { type emailRouter } from "@/server/api/routers/email";
 export interface Email {
   id: string;
   subject: string;
+  body: string;
   updatedAt: Date;
   sent: boolean;
 }
 
 const EmailPage = () => {
   // get list of emails
-  const { data: emails } = api.email.getAllEmails.useQuery();
+  const { data: emails, refetch } = api.email.getAllEmails.useQuery();
+  const getCommonMutationConfig = (toastMsg: string) => ({
+    onSuccess: async () => {
+      await refetch();
+      toast.success(toastMsg);
+    },
+    onError: (error: TRPCClientErrorLike<typeof emailRouter>) =>
+      toast.error(error.message),
+  });
+
+  const { mutate: createEmailMutate } = api.email.createEmail.useMutation(
+    getCommonMutationConfig("Email created successfully"),
+  );
+
+  const { mutate: deleteEmailMutate } = api.email.deleteEmail.useMutation(
+    getCommonMutationConfig("Email deleted successfully"),
+  );
+  const { mutate: updateEmailMutate } = api.email.updateEmail.useMutation(
+    getCommonMutationConfig("Email saved"),
+  );
 
   const [selectedEmail, setSelectedEmail] = useState<Email | undefined>();
+
   useEffect(() => {
-    if (emails) {
+    if (emails && !emails.find((email) => email.id === selectedEmail?.id)) {
       setSelectedEmail(emails[0]);
     }
-  }, [emails]);
+  }, [emails, selectedEmail]);
+
+  const createEmail = useCallback(
+    () => createEmailMutate(),
+    [createEmailMutate],
+  );
+  const deleteEmail = useCallback(() => {
+    if (selectedEmail) deleteEmailMutate({ id: selectedEmail.id });
+  }, [deleteEmailMutate, selectedEmail]);
+
+  const updateEmail = useCallback(
+    (subject: string, body: string) => {
+      if (selectedEmail?.id) {
+        updateEmailMutate({
+          id: selectedEmail.id,
+          subject: subject,
+          body: body,
+          to: [],
+        });
+      }
+    },
+    [selectedEmail?.id, updateEmailMutate],
+  );
 
   if (!emails) {
     return null;
@@ -32,8 +78,17 @@ const EmailPage = () => {
         emails={emails}
         selectedEmail={selectedEmail}
         setSelectedEmail={setSelectedEmail}
+        createEmail={createEmail}
+        deleteEmail={deleteEmail}
       />
-      <EmailClient />
+      <EmailClient
+        key={selectedEmail?.id}
+        email={selectedEmail}
+        updateEmail={updateEmail}
+        sendEmail={() => {
+          console.log("Sending email", selectedEmail);
+        }}
+      />
     </div>
   );
 };
