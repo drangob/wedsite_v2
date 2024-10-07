3a25cb43dd619e50dca6e6a97063cd3526bc95bd
Daniel Horbury <daniel.horbury@multiverse.io>
2024-10-07 23:35:56 +0100

    Make NextUI theme consistent with the site

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,5 +21,28 @@ export default withUt({
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              foreground: "#FFF",
+              50: "#ecfdf5",
+              100: "#d1fae5",
+              200: "#a7f3d0",
+              300: "#6ee7b7",
+              400: "#34d399",
+              500: "#10b981",
+              600: "#059669",
+              700: "#047857",
+              800: "#065f46",
+              900: "#064e3b",
+              DEFAULT: "#065F46",
+            },
+          },
+        },
+      },
+    }),
+  ],
 }) satisfies Config;
